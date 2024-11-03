43b67676af71dd7366343749100c9a0f7c171ff8
sanitha987 <94268783+georgekolath@users.noreply.github.com>
2024-11-03 08:40:57 +0530

    removed unwanted files

--- tests/test-2.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://getfares.com/');
-  await page.goto('https://getfares.com/SignIn');
-  
-  const singin = await page.getByRole('button', { name: 'Sign In' });
-  await expect(singin).toBeVisible();
-});
